a452a71fd1f073c147f4fb0fcb3e8adc678cec02
MennattallahIbrahim <116512615+MennattallahIbrahim@users.noreply.github.com>
2022-10-29 13:17:44 +0200

    Create 1. C program to perform input output of all basic data types.c

---
+++ C Programming assignments/Codeforwin/Basic C/1. C program to perform input output of all basic data types.c
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+void main (){
+	char input;
+	signed int input2; 
+	printf("Enter a character:");
+    scanf ("%c" , &input);
+	printf("You entered a character: %c \n" , input );
+	
+	printf("Enter a short signed value:");
+    scanf ("%d" , &input2);	
+	printf("You entered a short signed value: %d",input2);
+			
+}
