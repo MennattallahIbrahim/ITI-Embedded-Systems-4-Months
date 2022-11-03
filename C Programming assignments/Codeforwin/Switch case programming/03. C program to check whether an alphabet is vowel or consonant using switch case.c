0f583e529aee368cf10d6493145200dae557c0d1
MennattallahIbrahim <116512615+MennattallahIbrahim@users.noreply.github.com>
2022-11-03 21:17:23 +0200

    Create 03. C program to check whether an alphabet is vowel or consonant using switch case.c

---
+++ C Programming assignments/Codeforwin/Switch case programming/03. C program to check whether an alphabet is vowel or consonant using switch case.c
@@ -0,0 +1,30 @@
+#include<stdio.h> 
+void main() {
+	char alpha ;
+	printf("Input alphabet:");
+	scanf("%c",&alpha);
+	switch(alpha) {
+		case 'a' : printf("%c is vowel",alpha);
+		           break;
+		case 'e' : printf("%c is vowel",alpha);
+		           break;
+		case 'o' : printf("%c is vowel",alpha);
+		           break;
+		case 'u' : printf("%c is vowel",alpha);
+		           break;
+		case 'i' : printf("%c is vowel",alpha);
+		           break;
+		case 'A' : printf("%c is vowel",alpha);
+		           break;
+		case 'E' : printf("%c is vowel",alpha);
+		           break;
+		case 'O' : printf("%c is vowel",alpha);
+		           break;
+		case 'U' : printf("%c is vowel",alpha);
+		           break;
+		case 'I' : printf("%c is vowel",alpha);
+		           break;
+		default: printf("%c is consonant",alpha);
+		}
+		}
+		
