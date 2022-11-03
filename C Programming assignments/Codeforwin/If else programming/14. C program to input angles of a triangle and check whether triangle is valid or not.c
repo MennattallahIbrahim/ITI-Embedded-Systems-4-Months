775d485f92032e6bfbd012fe1177971c5cc49267
MennattallahIbrahim <116512615+MennattallahIbrahim@users.noreply.github.com>
2022-11-03 11:46:44 +0200

    Update 14. C program to input angles of a triangle and check whether triangle is valid or not.c

--- C Programming assignments/Codeforwin/If else programming/14. C program to input angles of a triangle and check whether triangle is valid or not.c
+++ C Programming assignments/Codeforwin/If else programming/14. C program to input angles of a triangle and check whether triangle is valid or not.c
@@ -1,14 +1,15 @@
 #include<stdio.h> 
 
 void main() {
-	int input1,input2,input3;
+	int input1,input2,input3,sum;
 	printf("Input first angle:");
 	scanf("%d", &input1);
 	printf("Input second angle:");
 	scanf("%d", &input2);
 	printf("Input third angle:");
 	scanf("%d", &input3);
-	if((input1+input2+input3==180)){
+	sum = input1+input2+input3;
+	if((sum == 180) && input1>0 && input2>0 && input3>0 ){
 		printf("The triangle is valid");
 			}
 			else{
