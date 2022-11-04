ee623879a3a21010b6cf8212161317f2ba4946e9
MennattallahIbrahim <116512615+MennattallahIbrahim@users.noreply.github.com>
2022-11-04 07:55:57 +0200

    Update 01. C program to check Least Significant Bit (LSB) of a number is set or not.c

--- C Programming assignments/Codeforwin/Bitwise operator programming/01. C program to check Least Significant Bit (LSB) of a number is set or not.c
+++ C Programming assignments/Codeforwin/Bitwise operator programming/01. C program to check Least Significant Bit (LSB) of a number is set or not.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h> 
+void main() {
+	int num;
+	printf("Input number:");
+	scanf("%d",&num);
+		if(num&1==1){
+			printf("Least Significant Bit of %d is set (1).",num);
+		}
+		else {
+			printf("Least Significant Bit of %d is set (0).",num);
+		}
+		}
