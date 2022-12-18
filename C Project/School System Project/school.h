50247d1e832c14008b988fa6835d21df946c876d
MennattallahIbrahim <116512615+MennattallahIbrahim@users.noreply.github.com>
2022-12-18 17:41:59 +0200

    Create school.h

---
+++ C Project/School System Project/school.h
@@ -0,0 +1,13 @@
+#ifndef SCHOOL_H
+#define SCHOOL_H
+
+uint8 main ();
+void INSERT_STUDENT(	);
+void DELETE_STUDENT(	);
+void STUDENT_EDIT(	);
+void STUDENT_LIST(	);
+void RANK_STUDENT(	);
+void STUDENT_SCORE(	);
+
+
+#endif
